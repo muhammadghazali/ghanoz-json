e7ba624becd4f7153d1cec2bcc87e17684c7f855
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-04-15 05:03:12 +0700

    Generate an app #5008018238607 #5008018238609

--- src/app/app.js
+++ src/app/app.js
@@ -1,35 +1,41 @@
 
 /**
- * Module dependencies.
+ * Eksternal modules
  */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var
+  express = require('express'),
+  http = require('http'),
+  path = require('path');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 
-app.configure('development', function(){
+// development only
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+}
+
+// development only
+if ('production' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// testing only
+if ('testing' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function (req, res) {
+  res.send('ghanozjson Web API!');
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
