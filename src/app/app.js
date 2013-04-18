06a5d050233335b4e2668adf66f9f0d48f477f3d
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-04-19 04:07:11 +0700

    Handle request with dummy response #5071900852401

--- src/app/app.js
+++ src/app/app.js
@@ -5,7 +5,8 @@
 var
   express = require('express'),
   http = require('http'),
-  path = require('path');
+  path = require('path'),
+  easyXML = require('easyxml');
 
 var app = module.exports = express();
 
@@ -32,8 +33,81 @@ if ('testing' == app.get('env')) {
   app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
 }
 
+/**
+ *
+ */
+function checkRequestedContentType (req, res, next) {
+  if (req.accepts('application/json') || req.accepts('application/xml'))
+    next();
+  else
+    res.send(406, 'Sorry, we only provide resources in JSON and XML.');
+}
+
+app.all('*', checkRequestedContentType);
+
 app.get('/', function (req, res) {
   res.send('ghanozjson Web API!');
+});
+
+app.get('/event/:id', function (req, res) {
+
+  if (req.params.id && req.accepts('application/json')) {
+    res.set('Content-Type', 'application/json');
+    res.json({
+      "url": "http://example.com/event/511206849146c42125000001",
+      "data": {
+        "title": "Free event",
+        "shortDescription": "Free event for students",
+        "times": [
+          {
+            "milliseconds": 1613028781634,
+            "fullFormat": "Thu Feb 11 2021 14:33:01 GMT+0700 (WIT)",
+            "year": 2021,
+            "month": 1,
+            "date": 11,
+            "day": 1
+          }
+        ],
+        "isPassed": false,
+        "rating": 3,
+        "lastUpdated": 1612928781634,
+        "timestamp": 1612928781634,
+        "id": "511206849146c42125000001"
+      }
+    });
+  } else if (req.params.id && req.accepts('application/xml')) {
+    res.set('Content-Type', 'application/xml');
+    easyXML.configure({
+      singularizeChildren: true,
+      underscoreAttributes: true,
+      rootElement: 'response',
+      dateFormat: 'ISO',
+      manifest: false,
+      indent: 2
+    });
+    res.send(200, easyXML.render({
+      "url": "http://example.com/event/511206849146c42125000001",
+      "event": {
+        "title": "Free event",
+        "shortDescription": "Free event for students",
+        "times": [
+          {
+            "milliseconds": 1613028781634,
+            "fullFormat": "Thu Feb 11 2021 14:33:01 GMT+0700 (WIT)",
+            "year": 2021,
+            "month": 1,
+            "date": 11,
+            "day": 1
+          }
+        ],
+        "isPassed": false,
+        "rating": 3,
+        "lastUpdated": 1612928781634,
+        "timestamp": 1612928781634,
+        "id": "511206849146c42125000001"
+      }
+    }));
+  }
 });
 
 http.createServer(app).listen(app.get('port'), function () {
