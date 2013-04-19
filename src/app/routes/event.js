8048dfe2d2abd9a0ee22c93c6e7d10f6c9a44885
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-04-20 05:54:32 +0700

    Enhance how the router handle the request based on the Accept header #5093225748399

--- src/app/routes/event.js
+++ src/app/routes/event.js
@@ -6,54 +6,83 @@ var easyXML = require('easyxml');
 
 exports.details = function (req, res) {
 
-  if (req.params.id && req.accepts('application/json')) {
-    var collection = req.mongodb.collection('events');
-    collection.findOne({_id: parseInt(req.params.id, 10)}, function (err, eventDoc) {
-      if (err) {
-        res.json(500, err.toString());
-      } else if (eventDoc) {
-        res.set('Content-Type', 'application/json');
-        var responseData = {
-          url: "http://localhost:3000/event" + req.params.id,
-          data: eventDoc
-        };
-        res.json(responseData);
-      } else if (eventDoc === null) {
-        res.set('Content-Type', 'application/json');
-        res.json(404, {message: "Not Found"});
-      }
-    });
-  } else if (req.params.id && req.accepts('application/xml')) {
-    res.set('Content-Type', 'application/xml');
-    easyXML.configure({
-      singularizeChildren: true,
-      underscoreAttributes: true,
-      rootElement: 'response',
-      dateFormat: 'ISO',
-      manifest: false,
-      indent: 2
-    });
-    res.send(200, easyXML.render({
-      "url": "http://example.com/event/511206849146c42125000001",
-      "event": {
-        "title": "Free event",
-        "shortDescription": "Free event for students",
-        "times": [
-          {
-            "milliseconds": 1613028781634,
-            "fullFormat": "Thu Feb 11 2021 14:33:01 GMT+0700 (WIT)",
-            "year": 2021,
-            "month": 1,
-            "date": 11,
-            "day": 1
-          }
-        ],
-        "isPassed": false,
-        "rating": 3,
-        "lastUpdated": 1612928781634,
-        "timestamp": 1612928781634,
-        "id": "511206849146c42125000001"
-      }
-    }));
+  if (!req.params.id) {
+    res.type('application/json');
+    res.json(400, {message: 'Request cannot be fulfilled due to bad syntax'});
   }
+
+  var eventID = parseInt(req.params.id, 10);
+
+  var collection = req.mongodb.collection('events');
+  collection.findOne({_id: eventID}, function (err, doc) {
+    if (err) {
+      res.format({
+        'application/json': function () {
+          res.json(500, err.toString());
+        },
+        'application/xml': function () {
+          // TODO it should be send an error in xml string
+          res.send(500, err.toString());
+        }
+      });
+    } else if (doc === null) {
+      res.format({
+        'application/json': function () {
+          res.json(400, err.toString());
+        },
+        'application/xml': function () {
+          // TODO it should be send an error in xml string
+          res.send(400, err.toString());
+        }
+      });
+
+    } else if (doc) {
+      res.format({
+        'application/json': function () {
+          var jsonResData = {
+            url: "http://localhost:3000/event" + eventID,
+            data: doc
+          };
+
+          res.json(jsonResData);
+        },
+        'application/xml': function () {
+
+          easyXML.configure({
+            singularizeChildren: true,
+            underscoreAttributes: true,
+            rootElement: 'response',
+            dateFormat: 'ISO',
+            manifest: false,
+            indent: 2
+          });
+
+          var xmlResData = easyXML.render({
+            "url": "http://example.com/event/1",
+            "event": {
+              "title": "Free event",
+              "shortDescription": "Free event for students",
+              "times": [
+                {
+                  "milliseconds": 1613028781634,
+                  "fullFormat": "Thu Feb 11 2021 14:33:01 GMT+0700 (WIT)",
+                  "year": 2021,
+                  "month": 1,
+                  "date": 11,
+                  "day": 1
+                }
+              ],
+              "isPassed": false,
+              "rating": 3,
+              "lastUpdated": 1612928781634,
+              "timestamp": 1612928781634,
+              "id": "511206849146c42125000001"
+            }
+          });
+
+          res.send(200, xmlResData);
+        }
+      });
+    }
+  });
 };
