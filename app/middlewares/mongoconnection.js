3f54e3b52634ab384d4afbfe72860c635cde70b7
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-04-27 19:11:03 +0700

    Passed the incoming arguments

--- app/middlewares/mongoconnection.js
+++ app/middlewares/mongoconnection.js
@@ -24,23 +24,45 @@ function connect (cb) {
   });
 }
 
+/**
+ * Get MongoDB connection
+ *
+ * @param {Object} req HTTP request object
+ * @param {Object} res HTTP response object
+ * @param {Function} next callback function
+ */
+function getDbConnection (req, res, next) {
+
+  connect(function (err, db) {
+    if (err)
+      res.send(500, 'Sorry, internal Error, please try again later');
+    else {
+      openedDb = db;
+      req.mongodb = db;
+      next();
+    }
+  });
+}
+
+/**
+ * Attach MongoDB connection
+ *
+ * @param {Object} req HTTP request object
+ * @param {Function} next callback function
+ */
+function passDbConnection (req, next) {
+  req.mongodb = openedDb;
+  next();
+}
+
 // expose the middleware
 module.exports.mongoConnection = function () {
 
   return function (req, res, next) {
     if (openedDb === null) {
-      connect(function (err, db) {
-        if (err)
-          res.send(500, 'Sorry, internal Error, please try again later');
-        else {
-          openedDb = db;
-          req.mongodb = db;
-          next();
-        }
-      });
+      getDbConnection(req, res, next);
     } else {
-      req.mongodb = openedDb;
-      next();
+      passDbConnection(req, next);
     }
   };
 };
