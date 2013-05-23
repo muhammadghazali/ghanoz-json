12c27bf67ce4d814336bbdd2a4fc5422793f7b7a
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-05-24 00:46:00 +0700

    Fallback to default db credentials if failed to load the production db credentials

--- app/configs/dbcredentials.js
+++ app/configs/dbcredentials.js
@@ -2,35 +2,32 @@
  * MongoDB connection information and crendentials
  */
 
-var config = {};
+
+// default configs
+var config = {
+  hostname: 'localhost',
+  port: 27017,
+  username: '',
+  password: '',
+  name: '',
+  db: 'ghanozjson'
+};
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    var env = JSON.parse(process.env.VCAP_SERVICES);
+    if (process.env.VCAP_SERVICES) {
+      var env = JSON.parse(process.env.VCAP_SERVICES);
+      config = env['mongodb-1.8'][0]['credentials'];
+    }
 
-    config = env['mongodb-1.8'][0]['credentials'];
     break;
 
   case 'development':
-    config = {
-      hostname: 'localhost',
-      port: 27017,
-      username: '',
-      password: '',
-      name: '',
-      db: 'ghanozjson'
-    };
+    config.db = 'ghanozjson';
     break;
 
   case 'testing':
-    config = {
-      hostname: 'localhost',
-      port: 27017,
-      username: '',
-      password: '',
-      name: '',
-      db: 'ghanozjson_test'
-    };
+    config.db = 'ghanozjson_test';
     break;
 }
 
